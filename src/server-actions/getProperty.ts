38ec8e83172041f4825ea4cce445374c21d0debd
ML510 <mr.mahesh.303@gmail.com>
2026-09-06 00:35:52 +0530

    Implement property details page with data fetching and formatting ( Click to property Card Open property Details )

---
+++ src/server-actions/getProperty.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/database/db";
+
+export async function getProperty(propertyId: string) {
+    try {
+        const property = await prisma.property.findUnique({
+            where: { id: propertyId },
+            include: {
+                owner: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true,
+                        image: true
+                    }
+                }
+            }
+        });
+
+        if (!property) {
+            return null;
+        }
+        return property;
+    } catch (error) {
+        console.error("Error fetching property:", error);
+        throw new Error("Failed to fetch property");
+        return null;
+    }
+
+}
